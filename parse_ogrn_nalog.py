a4d4aedc29ce4137842fa826086c79d94ca23faf
Дарья <ms.dluk@gmail.com>
2024-06-19 15:15:43 +0300

    add_new_5

--- parse_ogrn_nalog.py
+++ parse_ogrn_nalog.py
@@ -40,7 +40,7 @@ def create_browser():
         }
     }
     chrome_options = Options()
-    chrome_options.add_argument('--headless')
+    #chrome_options.add_argument('--headless')
     browser = webdriver.Chrome(seleniumwire_options=proxy_options, options=chrome_options)
     browser.set_window_size(1280, 11000)
 
@@ -58,63 +58,95 @@ def to_click(btn, browser):
     find_button.click()
 
 
-def get_info_ogrn(browser, wait, ogrn):
-    data = {
-        "status": None,
-        "inn": None,
-        "short_name": None,
-        "full_name": None,
-        "kpp": None,
-        "address": None,
-        "registration_date": None,
-        "okved": None,
-        "ogrn": None,
-        "boss_name": None,
-        "boss_post": None
-    }
+def get_info_ogrn(browser, wait, ogrn_or_inn):
+
     try:
-        browser.get(f'https://pb.nalog.ru/search.html#t=1718002842355&mode=search-all&queryAll={ogrn}&page=1&pageSize=10')
+        browser.get(f'https://pb.nalog.ru/search.html#t=1718002842355&mode=search-all&queryAll={ogrn_or_inn}&page=1&pageSize=10')
         wait.until(
             EC.visibility_of_all_elements_located(
                 (By.XPATH, '//div[@class="pb-card pb-card--clickable"]')))
         to_click(DIV_BTN, browser)
         wait.until(
             EC.visibility_of_all_elements_located(
-                (By.XPATH, '//*[@id="pnlCompanyLeftCol"]/div[3]/div/div[2]/a')))
+                (By.XPATH, '//span[@class = "pb-company-name"]')))
 
-        labels_and_xpaths = {
+        common_xpaths = {
             "status": "//span[@class='pb-subject-status pb-subject-status--active']",
             "inn": "//*[contains(text(), 'ИНН:')]/following-sibling::*[1]",
+            "okved": "//*[contains(text(), 'Основной вид деятельности (ОКВЭД):')]/following-sibling::*[1]"
+        }
+
+        labels_and_xpaths_org = {
+            **common_xpaths,
             "short_name": "//*[contains(text(), 'Сокращенное наименование:')]/following-sibling::*[1]",
             "full_name": "//*[contains(text(), 'Полное наименование:')]/following-sibling::*[1]",
             "kpp": "//*[contains(text(), 'КПП:')]/following-sibling::*[1]",
             "address": "//*[contains(text(), 'Адрес организации:')]/following-sibling::*[1]",
-            "registration_date": "//*[contains(text(), 'Дата регистрации:')]/following-sibling::*[1]",
-            "okved": "//*[contains(text(), 'Основной вид деятельности (ОКВЭД):')]/following-sibling::*[1]",
-            "ogrn": "//*[contains(text(), 'ОГРН:')]/following-sibling::*[1]"
+            "ogrn": "//*[contains(text(), 'ОГРН:')]/following-sibling::*[1]",
+            "registration_date": "//*[contains(text(), 'Дата регистрации:')]/following-sibling::*[1]"
         }
+
+        labels_and_xpaths_ip = {
+            **common_xpaths,
+            "name": "//*[contains(text(), 'ФИО:')]/following-sibling::*[1]",
+            "citizenship": "//*[contains(text(), 'Сведения о гражданстве:')]/following-sibling::*[1]",
+            "ogrnip": "//*[contains(text(), 'ОГРНИП:')]/following-sibling::*[1]",
+            "registration_date": "//*[contains(text(), 'Дата постановки на учёт:')]/following-sibling::*[1]"
+        }
+
+        if len(ogrn_or_inn) == 13 or len(ogrn_or_inn) == 10:
+            data = {
+                "status": None,
+                "inn": None,
+                "short_name": None,
+                "full_name": None,
+                "kpp": None,
+                "address": None,
+                "registration_date": None,
+                "okved": None,
+                "ogrn": None,
+                "boss_name": None,
+                "boss_post": None
+            }
+
+            labels_and_xpaths = labels_and_xpaths_org
+
+        elif len(ogrn_or_inn) == 15 or len(ogrn_or_inn) == 12:
+            data = {
+                "status": None,
+                "inn": None,
+                "name": None,
+                "citizenship": None,
+                "registration_date": None,
+                "okved": None,
+                "ogrnip": None
+            }
+
+            labels_and_xpaths = labels_and_xpaths_ip
 
         for key, xpath in labels_and_xpaths.items():
             try:
                 element = browser.find_element(By.XPATH, xpath)
                 data[key] = element.text
             except:
-                continue  # If element is not found, retain null in the data dictionary
+                continue
 
-        try:
-            boss_label = browser.find_element(By.XPATH,
-                                              "//*[contains(text(), 'Сведения о лице, имеющем право без доверенности действовать от имени юридического лица')]")
-            boss_name_number = boss_label.find_element(By.XPATH,
-                                                       ".//following::div[@class='pb-company-field-value']/span[@class='font-weight-bold']")
-            data["boss_name"] = boss_name_number.text
-            boss_post_element = boss_label.find_element(By.XPATH,
-                                                        ".//following::div[@class='pb-company-field-name' and contains(text(), 'Должность руководителя:')]/following-sibling::div[@class='pb-company-field-value']")
-            data["boss_post"] = boss_post_element.text
-        except:
-            pass
+        if len(ogrn_or_inn) == 13 or len(ogrn_or_inn) == 10:
+            try:
+                boss_label = browser.find_element(By.XPATH,
+                                                  "//*[contains(text(), 'Сведения о лице, имеющем право без доверенности действовать от имени юридического лица')]")
+                boss_name_number = boss_label.find_element(By.XPATH,
+                                                           ".//following::div[@class='pb-company-field-value']/span[@class='font-weight-bold']")
+                data["boss_name"] = boss_name_number.text
+                boss_post_element = boss_label.find_element(By.XPATH,
+                                                            ".//following::div[@class='pb-company-field-name' and contains(text(), 'Должность руководителя:')]/following-sibling::div[@class='pb-company-field-value']")
+                data["boss_post"] = boss_post_element.text
 
-    except Exception:
-        return None
+            except:
+                pass
+    except:
+        pass
+
     return data
 
 
@@ -202,3 +234,6 @@ def scrape_ogrn_info(ogrn_or_inn):
             time.sleep(retry_delay)
 
     return data
+
+
+print(scrape_ogrn_info('233910268521'))
